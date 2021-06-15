48a0b80a9bf6e90bb6df240dfdcb3e1929737149
Reinoptland <reinoptland@gmail.com>
2021-06-15 16:26:45 +0200

    Remove logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState, createContext } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
 import Afspraak from "./pages/Afspraak";
