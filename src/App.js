03dc8dfaabb1f2826306e23eb382a9ce64929319
Reinoptland <reinoptland@gmail.com>
2021-06-15 16:01:52 +0200

    Create context, pass values to Provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
@@ -6,51 +6,68 @@ import Afspraak from "./pages/Afspraak";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
+// context is an object
+const datePickerContext = createContext({});
+
 function App() {
+  console.log("WAT IS CONTEXT VOOR EEN DING:", datePickerContext);
   const [title, setTitle] = useState("");
   const [type, setType] = useState("");
   const [description, setDescription] = useState("");
   const [locatie, setLocatie] = useState("");
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/afspraak-maken">Prik een datum</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
+    <datePickerContext.Provider
+      value={{
+        title: title,
+        setTitle: setTitle,
+        type: type,
+        setType: setType,
+        description: description,
+        setDescription: setDescription,
+        locatie: locatie,
+        setLocatie: setLocatie,
+      }}
+    >
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/afspraak-maken">Prik een datum</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Switch>
-          <Route path="/afspraak-maken">
-            <Afspraak
-              title={title}
-              setTitle={setTitle}
-              description={description}
-              setDescription={setDescription}
-              type={type}
-              setType={setType}
-              locatie={locatie}
-              setLocatie={setLocatie}
-            />
-          </Route>
-          <Route path="/login">
-            <Login locatie={locatie} setLocatie={setLocatie} />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </header>
-    </div>
+          <Switch>
+            <Route path="/afspraak-maken">
+              <Afspraak
+                title={title}
+                setTitle={setTitle}
+                description={description}
+                setDescription={setDescription}
+                type={type}
+                setType={setType}
+                locatie={locatie}
+                setLocatie={setLocatie}
+              />
+            </Route>
+            <Route path="/login">
+              <Login locatie={locatie} setLocatie={setLocatie} />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </header>
+      </div>
+    </datePickerContext.Provider>
   );
 }
 
